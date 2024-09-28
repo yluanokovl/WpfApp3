f507552fb8b92143fd7573e2d2795f7c6212ad42
yluanokovl <yluanokovaleva15@gmail.com>
2024-09-28 23:41:45 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeBackgroundColor(object sender, RoutedEventArgs e)
+        {
+            var menuItem = (MenuItem)sender;
+            var color = (string)menuItem.Tag;
+            menu.Background = (System.Windows.Media.Brush)new System.Windows.Media.BrushConverter().ConvertFromString(color);
+            row.Background = (System.Windows.Media.Brush)new System.Windows.Media.BrushConverter().ConvertFromString(color);
+        }
+
+        private void ShowDeveloperInfo(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик: Ульяна Ковалёва");
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ShowStatusInfo(object sender, RoutedEventArgs e)
+        {
+            var menuItem = (MenuItem)sender;
+            var statusInfo = (string)menuItem.ToolTip;
+            statusTextBlock.Text = statusInfo;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
